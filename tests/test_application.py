ee8032e96023f3cf5977b2528a29961f1440cca5
Camillekiro <ckirongozi97@gmail.com>
2025-07-23 20:14:41 -0400

    tests for new storage systems

--- tests/test_application.py
+++ tests/test_application.py
@@ -6,7 +6,9 @@ import os
 #add app dir to the python path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'app'))
 
-from app.application import app, db, Draft
+from app.application import app, db, Draft, s3_client, ddb_client, S3_BUCKET_NAME, DDB_TABLE_NAME
+from instance.aws_s3_setup import initialize_s3
+from instance.aws_ddb_setup import initialize_dynamodb
 
 @pytest.fixture
 def client():
@@ -16,6 +18,9 @@ def client():
     with app.test_client() as client:
         with app.app_context():
             db.create_all()
+            # initialize AWS resources
+            initialize_s3(s3_client=s3_client, bucket_name=S3_BUCKET_NAME)
+            initialize_dynamodb(dynamodb_client=ddb_client, table_name=DDB_TABLE_NAME)
             yield client
             db.drop_all()
 
@@ -29,10 +34,24 @@ def sample_draft_data():
         "amateur_team": "Duke"
     }
 
+@pytest.fixture
+def duplicate_draft_data():
+    return {
+        "pick_number": "(77)",
+        "pro_team": "Boston Celtics",
+        "player_name": "John Doe",
+        "amateur_team": "Duke"
+    }
+
 
 def test_get_all_drafts(client):
     response = client.get('/api/v1/drafts')
     assert response.status_code == 200
+    # validate that all 3 storage systems are present
+    data = json.loads(response.data)
+    assert 'sqlite_draft_data' in data
+    assert 'dynamo_db_draft_data' in data
+    assert 's3_draft_data' in data
 
 
 def test_get_single_draft_record(client, sample_draft_data):
